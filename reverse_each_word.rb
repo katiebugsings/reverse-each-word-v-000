3ecaf439abe69a5e648c7ba63c30bdaf19fb58af
Katie Cassell <kcassell944@gmail.com>
2018-02-16 19:03:15 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(greeting)
-reversed = greeting.split(" ").collect do |word|
+greeting.split(" ").collect do |word|
     word.reverse
-end
- reversed.join(" ")
+end.join(" ")
 end
